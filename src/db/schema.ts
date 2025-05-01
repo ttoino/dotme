5e8aa1ef741231e59049af9bed795a0c7441a018
toino <me@toino.pt>
2025-05-01 20:21:45 +0100

    Fix user schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,15 +1,14 @@
 import { CV } from '@/types/cv';
-import { json, serial, singlestoreTable, timestamp, varchar } from 'drizzle-orm/singlestore-core';
+import { json, singlestoreTable, timestamp, varchar } from 'drizzle-orm/singlestore-core';
 
 const timestamps = {
   createdAt: timestamp().defaultNow().notNull(),
-  updatedAt: timestamp().defaultNow().onUpdateNow().notNull(),
+  updatedAt: timestamp().onUpdateNow().notNull(),
   deletedAt: timestamp(),
 }
 
 export const usersTable = singlestoreTable('users_table', {
-  id: serial().primaryKey(),
-  email: varchar({ length: 255 }).notNull(),
+  email: varchar({ length: 255 }).primaryKey(),
   password: varchar({ length: 255 }).notNull(),
   portfolio: json().$type<CV>().notNull(),
   ...timestamps,
