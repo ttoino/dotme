bf24862047763cfa4d9669a053a1ace9333a1634
toino <me@toino.pt>
2025-05-01 19:49:42 +0100

    Add db connection

---
+++ src/db/index.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+import mysql from "mysql2/promise";
+import { drizzle } from "drizzle-orm/singlestore";
+
+const client = mysql.createPool(process.env.DATABASE_URL!);
+const db = drizzle({
+    client,
+    casing: "snake_case",
+});
+
+export default db;
