9596a4aed18e2e801c1368648f0a9d37f715cb0e
diogotvf7 <diogotvf7@gmail.com>
2025-05-03 11:17:24 +0100

    Fixed some bugs

--- src/components/ui/profile-grid.tsx
+++ src/components/ui/profile-grid.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React from "react";
 import GridLayout from "react-grid-layout";
@@ -11,56 +11,54 @@ import PortfolioEntryExperience from "./portfolio-entry-experience";
 import PortfolioEntrySkill from "./portfolio-entry-skill";
 import PortfolioEntryRole from "./portfolio-entry-role";
 
-
 interface ProfileGridProps {
   user: User;
 }
 
 const ProfileGrid: React.FC<ProfileGridProps> = ({ user }) => {
   const portfolioInfo: portfolio_entry[] = user.portfolio_entries || [];
-  const experiences: Record<string, experience> = {}
-  const roles: Record<string, role> = {}
-  const skills: Record<string, skill> = {}
+  const experiences: Record<string, experience> = {};
+  const roles: Record<string, role> = {};
+  const skills: Record<string, skill> = {};
 
-  for(const portfolio_entry of portfolioInfo || []){
-    if( portfolio_entry.type == "skill"){
-      for(const skill of user.portfolio.skills || []){
-        if (skill.title == portfolio_entry.foreignId){
-          skills[skill.title] = skill
+  for (const portfolio_entry of portfolioInfo || []) {
+    if (portfolio_entry.type == "skill") {
+      for (const skill of user.portfolio.skills || []) {
+        if (skill.title == portfolio_entry.foreignId) {
+          skills[skill.title] = skill;
         }
       }
-    }else {
-      for(const area of user.portfolio.areas || []){
-        for(const experience of area.entries || []){
-          if(portfolio_entry.type == "experience" ){
-            if(experience.organization == portfolio_entry.foreignId){
-              experiences[experience.organization] = experience
-              console.log(experiences)
+    } else {
+      for (const area of user.portfolio.areas || []) {
+        for (const experience of area.entries || []) {
+          if (portfolio_entry.type == "experience") {
+            if (experience.organization == portfolio_entry.foreignId) {
+              experiences[experience.organization] = experience;
+              console.log(experiences);
             }
-          }else if(portfolio_entry.type == "role"){
-            for(const role of experience.roles || []){
-              if( role.title == portfolio_entry.foreignId){
-                roles[role.title] = role
+          } else if (portfolio_entry.type == "role") {
+            for (const role of experience.roles || []) {
+              if (role.title == portfolio_entry.foreignId) {
+                roles[role.title] = role;
               }
-            }   
+            }
           }
         }
       }
     }
   }
 
-  if(portfolioInfo.length === 0 || user === null){
-    return (
-      <div>Loading...</div>
-    )
+  if (portfolioInfo.length === 0 || user === null) {
+    return <div>Loading...</div>;
   }
 
   const styles: Record<string, string> = {
-    "experience" : "bg-accent border rounded shadow p-2 drag-handle cursor-move",
-    "skill" : "bg-primary border rounded shadow p-2 drag-handle cursor-move",
-    "role" : "bg-secondary border rounded shadow p-2 drag-handle cursor-move"
-  }
-
+    experience:
+      "bg-[#e2f0cb] dark:text-black border rounded shadow p-2 drag-handle cursor-move",
+    skill:
+      "bg-[#fcdce1]  dark:text-black border rounded shadow p-2 drag-handle cursor-move",
+    role: "bg-[#c8ceee]  dark:text-black border rounded shadow p-2 drag-handle cursor-move",
+  };
 
   const handleLayoutChange = (newLayout: any[]) => {
     newLayout.forEach((item) => {
@@ -71,11 +69,10 @@ const ProfileGrid: React.FC<ProfileGridProps> = ({ user }) => {
         width: item.w,
         height: item.h,
       };
-      
+
       updatePortfolioEntry(updatedEntry);
     });
   };
-  
 
   return (
     <div className="p-4">
@@ -89,33 +86,42 @@ const ProfileGrid: React.FC<ProfileGridProps> = ({ user }) => {
         isDraggable
         onLayoutChange={handleLayoutChange}
       >
-        {portfolioInfo.map(info => {
-        // Determine the component to render based on info.type
-        let content;
-        switch(info.type) {
-          case "experience":
-            content = <PortfolioEntryExperience experience={experiences[info.foreignId]} />;
-            break;
-          case "skill":
-            content = <PortfolioEntrySkill skill={skills[info.foreignId]} />;
-            break;
-          case "role":
-            content = <PortfolioEntryRole role={roles[info.foreignId]} />;
-            break;
-          default:
-            content = <div>No content available</div>;
-        }
+        {portfolioInfo.map((info) => {
+          // Determine the component to render based on info.type
+          let content;
+          switch (info.type) {
+            case "experience":
+              content = (
+                <PortfolioEntryExperience
+                  experience={experiences[info.foreignId]}
+                />
+              );
+              break;
+            case "skill":
+              content = <PortfolioEntrySkill skill={skills[info.foreignId]} />;
+              break;
+            case "role":
+              content = <PortfolioEntryRole role={roles[info.foreignId]} />;
+              break;
+            default:
+              content = <div>No content available</div>;
+          }
 
-        return (
-          <div
-            key={info.foreignId?.toString()}
-            data-grid={{ x: info.x, y: info.y, w: info.width, h: info.height }}
-            className={styles[info.type]}
-          >
-            {content}
-          </div>
-        );
-      })}
+          return (
+            <div
+              key={info.foreignId?.toString()}
+              data-grid={{
+                x: info.x,
+                y: info.y,
+                w: info.width,
+                h: info.height,
+              }}
+              className={styles[info.type]}
+            >
+              {content}
+            </div>
+          );
+        })}
       </GridLayout>
     </div>
   );
